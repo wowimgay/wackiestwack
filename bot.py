1453c7235b04bfb655280b4314d514ee442633a8
wowimgay <45774873+wowimgay@users.noreply.github.com>
2018-12-11 23:57:35 +0000

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 import discord
 from discord.ext import commands
+import asyncio
 
-TOKEN = 'NTIxNzU1NjUzNzE1MjYzNTAw.DvBVPQ.wUha3dSR86pZkMDh3apW_jMEWD8'#bot token
+TOKEN = 'NTIxNzU1NjUzNzE1MjYzNTAw.DvHV-A.nKerfcc06WvAkTAMe2amfYCIBM8'#bot token
 
 client = commands.Bot(command_prefix = "£")
 
+modLog = ""
 pengList = ""
 pengList = list(pengList)
 
@@ -13,10 +15,24 @@ async def on_ready():
     await client.change_presence(game=discord.Game(name="Collecting the big waifus"))
     print("Ready for SAUCE")#prepping itself
 
+@client.event
+async def on_message(message):
+    content = message.content
+    author = message.author
+    channel = message.channel
+    wack = str(content)
+    print(author)
+    print(str(author))
+    if str(author) in "Mudamaid4#1070":
+        if "high school dxd" in wack.lower():
+            await client.send_message(channel, "Claim for ALEEEEEEEEEXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+    await client.process_commands(message)
+
 @client.command()
-async def status(*args):
-    status = args
-    await client.change_presence(game=discord.Game(name = status))
+async def modlog(message):
+    global modLog
+    modLog = message.channel
+    await client.process_commands(message)
 
 @client.command()
 async def ping():
@@ -47,8 +63,34 @@ async def r():
     global pengList
 
     await client.say(pengList)
-
-
+'''
+@client.event
+async def on_message(message):
+    global modLog
+    author = message.author
+    content = message.content
+    wack = str(content)
+    if wack.lower() in "gay":
+        await client.send_message(modLog, "{}: {}".format(author, content))
+    await client.process_commands(message)
+'''
+'''
+@client.event
+async def on_message(message):
+    author = message.author
+    content = message.content
+    wack = list(content)
+    alsoWack = "-"
+    print(wack)
+    await client.process_commands(message)
+    for i in range(len(wack)):
+        if i % 2 == 0:
+            wack[i] = wack[i].upper()
+        else:
+            wack[i] = wack[i].lower()
+    wackFin = alsoWack.join(wack)
+    print(wackFin)
+'''
 '''
 @client.event
 async def on_message(message):
@@ -62,5 +104,6 @@ async def on_message_delete(message):
     content = message.content
     channel = message.channel
     await client.send_message(channel, "{}: {}".format(author, content))
+    await client.process_commands(message)
 '''
 client.run(TOKEN)
